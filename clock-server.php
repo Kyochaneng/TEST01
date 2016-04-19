e022416a0fc1b773275b352cf885e732cae7b183
kyochaneng <kyo.ishi@gmail.com>
2016-04-19 16:03:10 +0900

    コメント文を追加

--- clock-server.php
+++ clock-server.php
@@ -1,6 +1,8 @@
 <?php
+// MIMEタイプを出力
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
+// 現在時刻を表示
 for (;;){
 	$time = date('r');
 	echo "data: The server time is: {$time}\n\n";
