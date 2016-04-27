c821824913d81ac123de1c6e50cab016676469eb
kyochaneng <kyo.ishi@gmail.com>
2016-04-27 16:07:41 +0900

    二つのタイムゾーンを取得
    日本と英国の時間を取得し一個のデータとしてクラインアント側に SSE を使って送出する

--- clock-server.php
+++ clock-server.php
@@ -2,9 +2,10 @@
 // MIMEタイプを出力
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
+
+/** 今までコードはオリジナルとして残しておく
 // TIME ZONEの設定
 date_default_timezone_set('Asia/Tokyo');
-
 // 現在時刻を表示
 for (;;){
 	$time = date('r');
@@ -12,5 +13,27 @@ for (;;){
 	echo "data: The server another time is: {$time}\n\n";
 	ob_flush(); flush();
 	sleep(1);
+}
+*/
+
+// 二箇所のタイムゾーンから時刻を取り出し1秒毎にクライアントへ転送
+for(;;){
+	//東京時間の取得
+	date_default_timezone_set('Asia/Tokyo');	
+	$_time_Japan = date('r');
+	//ドイツ時間の取得
+	date_default_timezone_set('Europe/London');
+	$_time_England = date('r');	
+	//クライアントに両時刻を送出
+	sendMessage($_time_Japan, $_time_England);
+	//１秒間スリープ
+	sleep(1);
+}
+
+// 日本時刻とドイツ時刻を送出する関数:書式"time1:time2"
+function sendMessage($_time1, $_time2){
+	echo "data: $_time1:$_time2\n\n";
+	ob_flush();
+	flush();
 }
 ?>
